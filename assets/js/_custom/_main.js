1f4a67fed7c112cfbac41b533e6d18700140e144
Andrew Newhouse <andrewwn@gmail.com>
2018-04-16 20:42:36 -0400

    Feature: Adding image modal functionality (#77)
    * adding magnific popups to site
    
    * changing license & jquery & modal plugin to npm

---
+++ assets/js/_custom/_main.js
@@ -0,0 +1,39 @@
+/* ==========================================================================
+   jQuery plugin settings and other scripts
+   ========================================================================== */
+
+document.getElementsByTagName('hy-push-state')[0].addEventListener('hy-push-state-load', function() {
+
+  // add lightbox class to all image links
+  $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+
+  // Magnific-Popup options
+  $(".image-popup").magnificPopup({
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      // Will preload 0 - before current, and 1 after the current image
+      preload: [0,1] 
+    },
+    image: {
+      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
+    },
+    removalDelay: 500, // Delay in milliseconds before popup is removed
+    // Class that is added to body when popup is open.
+    // make it unique to apply your CSS animations just to this exact popup
+    mainClass: 'mfp-zoom-in',
+    callbacks: {
+      beforeOpen: function() {
+        // just a hack that adds mfp-anim class to markup
+        this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+      }
+    },
+    closeOnContentClick: true,
+    // allow opening popup on middle mouse click. 
+    // Always set it to true if you don't provide alternative source.
+    midClick: true 
+  });
+
+});
